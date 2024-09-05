b97147ffb1a74ad597bba8ef8b83ff1395b50548
wiqrug <papavasiliou.alexios@gmail.com>
2024-09-05 20:48:45 +0300

    working on secondpage

--- src/components/first-component-subcomps/SecondPage.jsx
+++ src/components/first-component-subcomps/SecondPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const SecondPage = () => {
-  return <>hello</>;
+  return (
+    <>
+      <h2>Defining a component </h2>
+      <p>
+        Traditionally when creating web pages, web developers marked up their
+        content and then added interaction by sprinkling on some JavaScript.
+        This worked great when interaction was a nice-to-have on the web. Now it
+        is expected for many sites and all apps. React puts interactivity first
+        while still using the same technology: a React component is a JavaScript
+        function that you can sprinkle with markup. Here’s what that looks like
+        (you can edit the example below):
+      </p>
+    </>
+  );
 };
 
 export default SecondPage;
